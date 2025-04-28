c186e650830a4de936ef31632c3ed73d43f4447a
yelly <kk3p1q1@gmail.com>
2025-04-28 16:30:06 +0900

    imp: 카드 팩 씬 - 팩 클릭 시 설명 표시

--- card_pack.gd
+++ card_pack.gd
@@ -15,4 +15,4 @@ func _ready() -> void:
 
 func _on_gui_input(event: InputEvent) -> void:
   if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-    card_pack_clicked.emit()
+    card_pack_clicked.emit(description_text)
--- hud.gd
+++ hud.gd
@@ -6,9 +6,9 @@ extends CanvasLayer
 @export var cardpack_scene: PackedScene
 
 var card_data = [
-  { "image": preload("res://images/card_pack_1.png"), "description": "첫 번째 카드 설명" },
-  { "image": preload("res://images/card_pack_2.png"), "description": "두 번째 카드 설명" },
-  { "image": preload("res://images/card_pack_3.png"), "description": "세 번째 카드 설명" }
+  { "image": preload("res://images/card_pack_1.png"), "description": "첫 번째 카드 설명: 123 123 123" },
+  { "image": preload("res://images/card_pack_2.png"), "description": "두 번째 카드 설명: 라라라 라라라랄" },
+  { "image": preload("res://images/card_pack_3.png"), "description": "세 번째 카드 설명: There is no harm." }
 ]
 
 
@@ -17,9 +17,9 @@ func _ready() -> void:
     var card_pack = cardpack_scene.instantiate()
     card_pack.card_texture = data["image"]
     card_pack.description_text = data["description"]
-    card_pack.gui_input.connect(_on_card_pack_gui_input)
+    card_pack.card_pack_clicked.connect(_on_card_pack_gui_input)
     hbox.add_child(card_pack)
 
 
-func _on_card_pack_gui_input(event: InputEvent) -> void:
-  label = event
+func _on_card_pack_gui_input(description) -> void:
+  label.text = description
