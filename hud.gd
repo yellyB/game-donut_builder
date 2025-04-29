72d7629a0e2a0e03a7a7bd5b06fbab965febb367
yelly <kk3p1q1@gmail.com>
2025-04-29 17:56:39 +0900

    imp: 돈이 표시될 TopBar 씬 생성

--- hud.gd
+++ hud.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
 
-@onready var hbox = $MarginContainer/VBoxContainer/HBoxContainer
+@onready var card_pack_grid = $MarginContainer/VBoxContainer/CardPackContainer
 @onready var label = $MarginContainer/VBoxContainer/HBoxContainer2/Description
 @export var cardpack_scene: PackedScene
 
 var card_data = [
-  { "image": preload("res://images/card_pack_1.png"), "description": "첫 번째 카드 설명: 123 123 123" },
-  { "image": preload("res://images/card_pack_2.png"), "description": "두 번째 카드 설명: 라라라 라라라랄" },
-  { "image": preload("res://images/card_pack_3.png"), "description": "세 번째 카드 설명: There is no harm." }
+  { "image": preload("res://images/card_pack_1.png"), "price": 1000, "description": "첫 번째 카드 설명: 123 123 123" },
+  { "image": preload("res://images/card_pack_2.png"), "price": 3000, "description": "두 번째 카드 설명: 라라라 라라라랄" },
+  { "image": preload("res://images/card_pack_3.png"), "price": 8000, "description": "세 번째 카드 설명: There is no harm." }
 ]
 
 
@@ -18,7 +18,7 @@ func _ready() -> void:
     card_pack.card_texture = data["image"]
     card_pack.description_text = data["description"]
     card_pack.card_pack_clicked.connect(_on_card_pack_gui_input)
-    hbox.add_child(card_pack)
+    card_pack_grid.add_child(card_pack)
 
 
 func _on_card_pack_gui_input(description) -> void:
