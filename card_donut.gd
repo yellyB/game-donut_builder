135131f94deeedd670391a1007554ae7c153b62e
yelly <kk3p1q1@gmail.com>
2025-05-08 10:44:31 +0900

    imp: 클릭한 도넛 카드 사라짐

--- card_donut.gd
+++ card_donut.gd
@@ -12,7 +12,8 @@ func can_overlap_with(other_card: Node) -> bool:
   return false
 
 
-# todo: 도넛을 손님 카드에 충돌 시 다음 동작 수행하도록 수
+# todo: 도넛을 손님 카드에 충돌 시 다음 동작 수행하도록 수정
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
   if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
     card_clicked.emit()
+    queue_free()
