2a8fe71fa96a45c44eb923cea47c79504bdba3fc
yelly <kk3p1q1@gmail.com>
2025-07-24 16:55:30 +0900

    feat: 도넛 카드 - 여러 종류 도넛 생성(TSK-31)
    Closes #24

--- global/constants.gd
+++ global/constants.gd
@@ -3,6 +3,7 @@ extends Node
 
 enum CardType { MATERIAL, DONUT, CUSTOMER }
 enum MaterialType { MILK, SUGAR, FLOUR }
+enum DonutType { MILK, STRAWBERRY, CHOCOLATE }
 
 
 # Called when the node enters the scene tree for the first time.
