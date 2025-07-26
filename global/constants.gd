3d944abcfdc791215694608de0959be90e01595f
yelly <kk3p1q1@gmail.com>
2025-07-27 02:01:27 +0900

    feat: 제작 리스트에서 보유한 재료에 따라 가능 여부 표시(#30)
    Closes #30

--- global/constants.gd
+++ global/constants.gd
@@ -2,9 +2,8 @@ extends Node
 
 
 enum CardType { MATERIAL, DONUT, CUSTOMER }
-enum MaterialType { MILK, SUGAR, FLOUR }
+enum MaterialType { MILK, SUGAR, FLOUR, STRAWBERRY, CHOCOLATE, MINT }
 enum DonutType { MILK, STRAWBERRY, CHOCOLATE, MINT }
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- hud.gd
+++ hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 const CardDonut = preload("res://card_donut.gd")
+const CardMaterial = preload("res://card_meterial.gd")
 @onready var card_pack_grid = $MarginContainer/VBoxContainer/CardPackContainer
 @onready var description_label = $MarginContainer/VBoxContainer/FooterContainer/Description
 @onready var money_label = $TopBar/HBoxContainer/MoneyContainer/MoneyLabel
@@ -15,6 +16,7 @@ var card_data = [
 ]
 
 
+#region Godot's built-in functions
 func _ready() -> void:
   _add_craft_item_nodes_to_list()
   TimerManager.time_updated.connect(_on_time_updated)
@@ -25,22 +27,28 @@ func _ready() -> void:
     card_pack.description_text = data["description"]
     card_pack.card_pack_clicked.connect(_on_card_pack_gui_input)
     card_pack_grid.add_child(card_pack)
+#endregion
 
 
-func _add_craft_item_nodes_to_list() -> void:
-  var all_donut_data = CardDonut.get_all_donut_data()
-  for donut_type_enum in all_donut_data:
-    var donut_info = all_donut_data[donut_type_enum]
-    var button = Button.new()
-    button.text = donut_info["name"] # todo: 레시피 정보도 추가
-    button.custom_minimum_size = Vector2(0, 80)
-    
-    var donut_type_string = Constants.DonutType.find_key(donut_type_enum)
-    button.pressed.connect(_on_craft_item_pressed.bind(donut_type_string))
-    
-    craft_list_vbox.add_child(button)
+#region Public functions
+func initialize(grid_manager_node: Node) -> void:
+  grid_manager = grid_manager_node
+  _refresh_craft_list()
 
 
+func update_money_display():
+  money_label.text = str(GameState.money)
+
+
+func update_timer_label(new_time) -> void:
+  var timer_label = $TopBar/HBoxContainer/TimerContainer/TimerLabel
+  var minutes = new_time / 60
+  var seconds = new_time % 60
+  timer_label.text = "%02d:%02d" % [minutes, seconds]
+#endregion
+
+
+#region Signal handlers
 func _on_card_pack_gui_input(description) -> void:
   description_label.text = description
 
@@ -52,17 +60,6 @@ func _on_moeny_increase(price: int):
   
 func _on_time_updated(new_time: int) -> void:
   update_timer_label(new_time)
-  
-
-func update_timer_label(new_time) -> void:
-  var timer_label = $TopBar/HBoxContainer/TimerContainer/TimerLabel
-  var minutes = new_time / 60
-  var seconds = new_time % 60
-  timer_label.text = "%02d:%02d" % [minutes, seconds]
-
-
-func update_money_display():
-  money_label.text = str(GameState.money)
 
 
 func _on_button_pressed() -> void:
@@ -75,11 +72,14 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
   grid_manager.spawn_cards("CUSTOMER", 1)
-  
 
 
 func _on_craft_button_pressed() -> void:
-  craft_list_container.visible = not craft_list_container.visible
+  if craft_list_container.visible:
+    craft_list_container.visible = false
+  else:
+    _refresh_craft_list()
+    craft_list_container.visible = true
 
 
 func _on_craft_item_pressed(donut_type: String) -> void:
@@ -87,7 +87,67 @@ func _on_craft_item_pressed(donut_type: String) -> void:
   var donut_enum_val = Constants.DonutType.get(donut_type)
   # todo: 실제 제작 로직 추가 (예: grid_manager.craft_item(donut_enum_val))
   print("제작 아이템 클릭됨: ", donut_type, " (", donut_enum_val, ")")
+#endregion
 
 
-func initialize(grid_manager_node: Node) -> void:
-  grid_manager = grid_manager_node
+#region Helper functions
+func _add_craft_item_nodes_to_list() -> void:
+  var all_donut_data = CardDonut.get_all_donut_data()
+  for donut_type_enum in all_donut_data:
+    var donut_info = all_donut_data[donut_type_enum]
+    var button = Button.new()
+    
+    button.text = donut_info["name"]
+    button.custom_minimum_size = Vector2(0, 80)
+    
+    var donut_type_string = Constants.DonutType.find_key(donut_type_enum)
+    button.set_meta("donut_type_string", donut_type_string) # 버튼에 데이터 저장
+    button.pressed.connect(_on_craft_item_pressed.bind(donut_type_string))
+    
+    craft_list_vbox.add_child(button)
+
+
+func _get_recipe_text(donut_info: Dictionary, current_materials: Dictionary) -> String:
+  var recipe = donut_info["recipe"]
+  var recipe_text_parts = []
+  var required_materials = {}
+  for material_enum in recipe:
+    required_materials[material_enum] = required_materials.get(material_enum, 0) + 1
+    
+  for material_enum in required_materials:
+    var material_name = CardMaterial.get_all_material_data()[material_enum]["name"]
+    var have_count = current_materials.get(material_enum, 0)
+    var need_count = required_materials[material_enum]
+    recipe_text_parts.append("%s: %d/%d" % [material_name, have_count, need_count])
+    
+  return "%s\n(%s)" % [donut_info["name"], ", ".join(recipe_text_parts)]
+
+
+func _refresh_craft_list() -> void:
+  var current_materials = grid_manager.get_current_material_counts()
+  
+  for button in craft_list_vbox.get_children():
+    var donut_type_string = button.get_meta("donut_type_string")
+    var donut_type_enum = Constants.DonutType.get(donut_type_string)
+    var donut_info = CardDonut.get_all_donut_data()[donut_type_enum]
+    var recipe = donut_info["recipe"]
+    
+    button.text = _get_recipe_text(donut_info, current_materials)
+    
+    var can_craft = _is_craftable(recipe, current_materials)
+    button.disabled = not can_craft
+
+
+func _is_craftable(recipe: Array, available_materials: Dictionary) -> bool:
+  var required = {}
+  # 레시피에 필요한 재료 개수 카운트
+  for material in recipe:
+    required[material] = required.get(material, 0) + 1
+  
+  # 필요한 재료가 충분히 있는지 확인
+  for material in required:
+    if available_materials.get(material, 0) < required[material]:
+      return false
+      
+  return true
+#endregion
