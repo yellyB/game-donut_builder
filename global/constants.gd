5f2d8ab79064126c302e82a43f844e266e596444
yelly <kk3p1q1@gmail.com>
2025-07-27 19:17:20 +0900

    temp

--- global/constants.gd
+++ global/constants.gd
@@ -7,6 +7,9 @@ enum DonutType { MILK, STRAWBERRY, CHOCOLATE, MINT }
 enum DonutRarity { COMMON, RARE, EPIC }
 enum DonutTag { BASIC, SWEET, FRUITY, CHOCOLATE, MINTY, FRESH }
 
+enum CustomerType { WOMAN, MAN }
+enum OrderType { DONUT_TYPE, DONUT_TAG }
+
 const DONUT_DATA = {
   DonutType.MILK: {
     "name": "우유도넛",
@@ -47,36 +50,51 @@ const DONUT_DATA = {
 }
 
 const MATERIAL_DATA = {
-	MaterialType.MILK: {
-		"name": "우유",
-		"texture": preload("res://images/card/meterial/card_meterial_milk.png"),
-		"price": 10
-	},
-	MaterialType.SUGAR: {
-		"name": "설탕",
-		"texture": preload("res://images/card/meterial/card_meterial_sugar.png"),
-		"price": 10
-	},
-	MaterialType.FLOUR: {
-		"name": "밀가루",
-		"texture": preload("res://images/card/meterial/card_meterial_flour.png"),
-		"price": 15
-	},
-	MaterialType.STRAWBERRY: {
-		"name": "딸기",
-		"texture": preload("res://images/card/meterial/card_meterial_strawberry.png"),
-		"price": 30
-	},
-	MaterialType.CHOCOLATE: {
-		"name": "초콜릿",
-		"texture": preload("res://images/card/meterial/card_meterial_chocolate.png"),
-		"price": 35
-	},
-	MaterialType.MINT: {
-		"name": "민트",
-		"texture": preload("res://images/card/meterial/card_meterial_mint.png"),
-		"price": 50
-	}
+  MaterialType.MILK: {
+    "name": "우유",
+    "texture": preload("res://images/card/meterial/card_meterial_milk.png"),
+    "price": 10
+  },
+  MaterialType.SUGAR: {
+    "name": "설탕",
+    "texture": preload("res://images/card/meterial/card_meterial_sugar.png"),
+    "price": 10
+  },
+  MaterialType.FLOUR: {
+    "name": "밀가루",
+    "texture": preload("res://images/card/meterial/card_meterial_flour.png"),
+    "price": 15
+  },
+  MaterialType.STRAWBERRY: {
+    "name": "딸기",
+    "texture": preload("res://images/card/meterial/card_meterial_strawberry.png"),
+    "price": 30
+  },
+  MaterialType.CHOCOLATE: {
+    "name": "초콜릿",
+    "texture": preload("res://images/card/meterial/card_meterial_chocolate.png"),
+    "price": 35
+  },
+  MaterialType.MINT: {
+    "name": "민트",
+    "texture": preload("res://images/card/meterial/card_meterial_mint.png"),
+    "price": 50
+  }
+}
+
+const CUSTOMER_DATA = {
+  CustomerType.WOMAN: {
+    "name": "여자 손님",
+    "texture": preload("res://images/card_customer.png"),
+    "patience": 60,
+    "order": null
+  },
+  CustomerType.MAN: {
+    "name": "남자 손님",
+    "texture": preload("res://images/card_customer.png"),
+    "patience": 50,
+    "order": null
+  }
 }
 
 # Called when the node enters the scene tree for the first time.
